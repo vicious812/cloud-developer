fc2e71f326370df3993ea4f951468551160d8f57
vicious812 <aloysius.rotor@gmail.com>
2019-12-26 14:39:02 -0800

    AWS Ready

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,12 +1,12 @@
 export const config = {
   "dev": {
-    "username": "udgarotor",
-    "password": "vF]wY13dE)=V",
-    "database": "udgarotor",
-    "host": "udgarotor.ci04bdm2gooo.us-west-1.rds.amazonaws.com",
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres",
-    "aws_region": "us-west-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-arotor-dev",
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
   }
 }
