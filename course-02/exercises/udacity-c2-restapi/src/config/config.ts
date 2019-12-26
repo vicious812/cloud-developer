6d061a2daf07e5c573c67364ceff640a08cbdbec
vicious812 <aloysius.rotor@gmail.com>
2019-12-26 13:35:35 -0800

    validated connection to AWS S3 and RDS
    validated connection to AWS S3 and RDS

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,12 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udgarotor",
+    "password": "vF]wY13dE)=V",
+    "database": "udgarotor",
+    "host": "udgarotor.ci04bdm2gooo.us-west-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-west-2",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+    "aws_media_bucket": "udagram-arotor-dev",
   }
 }
